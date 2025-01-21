8870f5cfc5bf7e20de4d16d2db84059fa1c00654
Aditya <147301021+adityajha2005@users.noreply.github.com>
2025-01-21 11:50:12 +0530

    minor updates

--- src/components/ScrollButton.tsx
+++ src/components/ScrollButton.tsx
@@ -15,13 +15,10 @@ const ScrollButton = () => {
             setVisible(false);
         }
     };
-
     const scrollToTop = () => {
         window.scrollTo({
             top: 0,
             behavior: "smooth"
-            /* you can also use 'auto' behaviour
-         in place of 'smooth' */
         });
     };
 
